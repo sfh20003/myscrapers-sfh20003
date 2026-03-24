f7f116328cb45de729660f57d427d781364a25e6
Faizan H <syed.f.2.hussain@uconn.edu>
2026-03-24 17:32:05 -0400

    Update main.py

--- cloud_function/materialize-master/main.py
+++ cloud_function/materialize-master/main.py
@@ -27,7 +27,7 @@ RUN_ID_PLAIN_RE = re.compile(r"^\d{14}$")        # 20251026170002
 # Stable CSV schema for students
 CSV_COLUMNS = [
     "post_id", "run_id", "scraped_at",
-    "price", "year", "make", "model", "mileage",
+    "price", "year", "make", "model", "mileage", "color",
     "source_txt"
 ]
 
